b82c0e46afd94cd6b8ef75ba3dbf74af64112ece
thiagomonteiro03 <thiagomonteiro.trab@outlook.com>
2021-02-17 15:50:46 -0300

    layout home criado

---
+++ app/src/main/java/com/example/events/data/model/Event.kt
@@ -0,0 +1,10 @@
+package com.example.events.data.model
+
+class Event(     val date : Long,
+                      val description: String,
+                      val image: String,
+                      val longitude : Double,
+                      val latitude : Double,
+                      val price : Double,
+                      val title : String,
+                      val id : String)
--- app/src/main/java/com/example/events/presentation/events/EventsActivity.kt
+++ app/src/main/java/com/example/events/presentation/events/EventsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.events
+package com.example.events.presentation.events
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.events.R
 
-class MainActivity : AppCompatActivity() {
+class EventsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_events)
     }
 }
