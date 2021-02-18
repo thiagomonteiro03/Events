052e2c90a9ff9ec5591d70f0042b98352f86bfbb
thiagomonteiro03 <thiagomonteiro.trab@outlook.com>
2021-02-18 12:11:06 -0300

    adicionando verificação de atualizacoes google service

--- app/src/main/java/com/example/events/presentation/events/EventsActivity.kt
+++ app/src/main/java/com/example/events/presentation/events/EventsActivity.kt
@@ -1,14 +1,16 @@
 package com.example.events.presentation.events
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.events.R
-import com.example.events.data.model.Event
 import com.example.events.presentation.details.EventDetailsActivity
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GooglePlayServicesUtil
 import kotlinx.android.synthetic.main.activity_events.*
 
 class EventsActivity : AppCompatActivity() {
@@ -17,6 +19,17 @@ class EventsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_events)
 
         val viewModel: EventsViewModel = ViewModelProviders.of(this).get(EventsViewModel::class.java)
+
+        val status : Int = GooglePlayServicesUtil.isGooglePlayServicesAvailable(applicationContext);
+        if(status == ConnectionResult.SUCCESS) {
+
+        }else if(status == ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED){
+            Toast.makeText(
+                applicationContext,
+                R.string.att_google_play,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
 
         viewModel.eventsLiveData.observe(this, Observer{
             it?.let { events ->
